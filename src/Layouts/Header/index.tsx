19ac816341167467fce68d819beedb2611419870
hhvrc <github@heavenvr.tech>
2022-10-19 17:22:00 +0200

    Fix layouts

--- src/Layouts/Header/index.tsx
+++ src/Layouts/Header/index.tsx
@@ -1,15 +1,28 @@
-import { Toolbar, AppBar, Button } from "@mui/material";
-import 'Assets/Styles/App.css';
+import { Box, Toolbar, AppBar, Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 function Header(): JSX.Element {
   return (
-    <AppBar>
-        <Toolbar>
-            <Button color="inherit" component={NavLink} to="/" sx={{ fontSize: 20, fontWeight: 'bold', textTransform: 'none' }}> ZapMe </Button>
-            <Button color="inherit" component={NavLink} to="/signin" sx={{ fontSize: 20, fontWeight: 'bold', textTransform: 'none' }}> Sign In </Button>
-            <Button color="inherit" component={NavLink} to="/signup" sx={{ fontSize: 20, fontWeight: 'bold', textTransform: 'none' }}> Sign Up </Button>
-        </Toolbar>
+    <AppBar position="sticky" sx={{ flexGrow: 1 }}>
+      <Toolbar>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="/"
+            sx={{ fontSize: 20, fontWeight: 'bold', textTransform: 'none' }}
+          >
+            ZapMe
+          </Button>
+          <Box mx={{ flexGrow: 1 }}/>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="/signin"
+            sx={{ fontSize: 20, fontWeight: 'bold', textTransform: 'none' }}
+          >
+            Login
+          </Button>
+      </Toolbar>
     </AppBar>
   );
 }
