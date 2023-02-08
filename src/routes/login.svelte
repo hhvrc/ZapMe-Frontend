0a667911db3f2b7fbdf4c2e604c892f2b47e21a4
hhvrc <github@heavenvr.tech>
2023-02-08 23:11:27 +0100

    fix login and register page

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { goto } from '@roxi/routify';
     import { AuthenticationApi } from '$api/index';
-    import CenteredRect from '$cmp/CenteredRect.svelte';
 
     const accountApi = new AuthenticationApi();
 
@@ -9,7 +8,7 @@
     let username = '';
     let password = '';
 
-    async function submitForm() {
+    async function handleSubmit() {
         accountApi.authSignIn({username, password})
         .then((response) => {
             console.log(response);
@@ -28,24 +27,114 @@
     <title>ZapMe - {title}</title>
 </svelte:head>
 
-<CenteredRect>
-    Sign In
+<form on:submit|preventDefault={handleSubmit}>
+    <h3>Sign In</h3>
 
-    <form on:submit|preventDefault={submitForm}>
-        <label for="username">Username</label>
-        <input type="username" name="username" bind:value={username} />
+    <label for="username">Username</label>
+    <input type="username" placeholder="Username" name="username" bind:value={username}>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" bind:value={password} />
-        <br>
-        <a href="/reset-password">Forgot Password?</a>
-        <br>
-        <br>
-        <button type="submit" disabled='{!validateForm(username, password)}'>Login</button>
-    </form>
+    <label for="password">Password</label>
+    <input type="password" placeholder="Password" name="password" bind:value={password}>
 
-    <a href="/register">Register</a>
-</CenteredRect>
+    <button type="submit" disabled='{!validateForm(username, password)}'>Sign In</button>
+
+    <div class="social">
+        <button><i></i>Google</button>
+        <button><i></i>Twitter</button>
+    </div>
+</form>
+
+<a href="/register">Register</a>
 
 <style>
+    *,
+    *:before,
+    *:after{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    form{
+        height: fit-content;
+        width: fit-content;
+        min-width: 500px;
+        background-color: rgba(255,255,255,0.13);
+        position: absolute;
+        transform: translate(-50%,-50%);
+        top: 50%;
+        left: 50%;
+        border-radius: 10px;
+        border: 2px solid rgba(255,255,255,0.1);
+        box-shadow: 0 0 40px rgba(8,7,16,0.6);
+        padding: 50px 35px;
+    }
+    form *{
+        font-family: 'Poppins',sans-serif;
+        color: #ffffff;
+        letter-spacing: 0.5px;
+        outline: none;
+        border: none;
+    }
+    form h3{
+        font-size: 42px;
+        font-weight: 500;
+        line-height: 42px;
+        text-align: center;
+    }
+
+    form label{
+        display: block;
+        margin-top: 30px;
+        font-size: 16px;
+        font-weight: 500;
+    }
+    form input{
+        display: block;
+        height: 50px;
+        width: 100%;
+        background-color: rgba(255,255,255,0.07);
+        border-radius: 3px;
+        padding: 0 10px;
+        margin-top: 8px;
+        font-size: 14px;
+        font-weight: 300;
+    }
+    ::placeholder{
+        color: #e5e5e5;
+    }
+    form div {
+        margin-top: 30px;
+    }
+    form button{
+        margin-top: 30px;
+        width: 100%;
+        background-color: #ffffff;
+        color: #080710;
+        padding: 15px 0;
+        font-size: 18px;
+        font-weight: 600;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .social{
+        display: flex;
+        gap: 10px;
+    }
+    .social button{
+        margin-top: 20px;
+        background: red;
+        width: 150px;
+        border-radius: 3px;
+        padding: 5px 10px 10px 5px;
+        background-color: rgba(255,255,255,0.27);
+        color: #eaf0fb;
+        text-align: center;
+    }
+    .social button:hover{
+        background-color: rgba(255,255,255,0.47);
+    }
+    .social i{
+        margin-right: 4px;
+    }
 </style>
--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { goto } from '@roxi/routify';
     import { AccountApi } from '$api/index';
-  import CenteredRect from '$cmp/CenteredRect.svelte';
 
     const accountApi = new AccountApi();
 
@@ -9,8 +8,9 @@
     let username = '';
     let email = '';
     let password = '';
+    let confirmedPassword = '';
 
-    async function register() {
+    async function handleSubmit() {
         accountApi.createAccount({username, email, password})
         .then((response) => {
             console.log(response);
@@ -20,33 +20,112 @@
             console.log(error);
         });
     }
+    function validateForm(username:string, password:string, email:string, confirmedPassword:string) {
+        return username.length > 0 && password.length > 0 && email.length > 0 && password === confirmedPassword;
+    }
 </script>
 
 <svelte:head>
     <title>ZapMe - {title}</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </svelte:head>
 
-<CenteredRect>
-    Sign In
+<form on:submit|preventDefault={handleSubmit}>
+    <h3>Register</h3>
 
-    <form on:submit|preventDefault={register}>
-        <label for="username">Username</label>
-        <input type="username" name="username" bind:value={username} />
+    <label for="username">Username</label>
+    <input type="username" placeholder="Username" name="username" bind:value={username}>
 
-        <label for="email">Email</label>
-        <input type="email" name="email" bind:value={email} />
+    <label for="email">Email</label>
+    <input type="email" placeholder="Email" name="email" bind:value={email}>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" bind:value={password} />
+    <label for="password">Password</label>
+    <input type="password" placeholder="Password" name="password" bind:value={password}>
 
-        <label for="password">Confirm Password</label>
-        <input type="password" name="password" bind:value={password} />
+    <label for="password">Confirm Password</label>
+    <input type="password" placeholder="Password" name="password" bind:value={confirmedPassword}>
 
-        <button type="submit">Register</button>
-    </form>
+    <div class="g-recaptcha" data-sitekey="{import.meta.env.VITE_RECAPTCHA_SITEKEY}"></div>
 
-    <a href="/login">Login</a>
-</CenteredRect>
+    <button type="submit" disabled='{!validateForm(username, password, email, confirmedPassword)}'>Register</button>
+</form>
+
+<a href="/login">Login</a>
 
 <style>
+    *,
+    *:before,
+    *:after{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    form{
+        height: fit-content;
+        width: fit-content;
+        min-width: 500px;
+        background-color: rgba(255,255,255,0.13);
+        position: absolute;
+        transform: translate(-50%,-50%);
+        top: 50%;
+        left: 50%;
+        border-radius: 10px;
+        border: 2px solid rgba(255,255,255,0.1);
+        box-shadow: 0 0 40px rgba(8,7,16,0.6);
+        padding: 50px 35px;
+    }
+    form *{
+        font-family: 'Poppins',sans-serif;
+        color: #ffffff;
+        letter-spacing: 0.5px;
+        outline: none;
+        border: none;
+    }
+    form h3{
+        font-size: 42px;
+        font-weight: 500;
+        line-height: 42px;
+        text-align: center;
+    }
+
+    form label{
+        display: block;
+        margin-top: 30px;
+        font-size: 16px;
+        font-weight: 500;
+    }
+    form input{
+        display: block;
+        height: 50px;
+        width: 100%;
+        background-color: rgba(255,255,255,0.07);
+        border-radius: 3px;
+        padding: 0 10px;
+        margin-top: 8px;
+        font-size: 14px;
+        font-weight: 300;
+    }
+    ::placeholder{
+        color: #e5e5e5;
+    }
+    form div {
+        margin-top: 30px;
+    }
+    form .g-recaptcha{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 304px;
+    }
+    form button{
+        margin-top: 30px;
+        width: 100%;
+        background-color: #ffffff;
+        color: #080710;
+        padding: 15px 0;
+        font-size: 18px;
+        font-weight: 600;
+        border-radius: 5px;
+        cursor: pointer;
+    }
 </style>
