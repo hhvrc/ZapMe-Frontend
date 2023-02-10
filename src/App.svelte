6c245e8c9ab5202aaed80c397e345f4a84a3b84d
hhvrc <github@heavenvr.tech>
2023-02-10 10:30:30 +0100

    Create nice scrollbar

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
 	import routes from '../.routify/routes.default.js';
   import Header from '$cmp/Header.svelte';
   import Footer from '$cmp/Footer.svelte';
+
+  let mainClass: string|null = null;
 </script>
 
 <Header/>
-<main>
+<main class={mainClass} on:mouseenter={() => mainClass = "scrollable"} on:mouseleave={() => mainClass = null}>
   <Router {routes}/>
 </main>
 <Footer/>
@@ -15,6 +17,23 @@
   main {
     height: 100%;
     width: auto;
+    overflow-y: hidden;
+  }
+  .scrollable {
     overflow-y: auto;
+  }
+
+  /* Custom scrollbar */
+  ::-webkit-scrollbar {
+    width: 14px;
+  }
+  ::-webkit-scrollbar-track {
+    visibility: hidden;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #88888888;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #bbbbbbbb;
   }
 </style>
