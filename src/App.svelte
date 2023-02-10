7db2cc3812868b80d22afc27eee55ac512e4df7d
hhvrc <github@heavenvr.tech>
2023-02-10 11:10:49 +0100

    Only CSS UwU

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,12 @@
 	import routes from '../.routify/routes.default.js';
   import Header from '$cmp/Header.svelte';
   import Footer from '$cmp/Footer.svelte';
-
-  let mainClass: string|null = null;
 </script>
 
 <Header/>
-<main class={mainClass} on:mouseenter={() => mainClass = "scrollable"} on:mouseleave={() => mainClass = null}>
+<main>
   <Router {routes}/>
+  <div class="scroll-cover"/>
 </main>
 <Footer/>
 
@@ -17,23 +16,28 @@
   main {
     height: 100%;
     width: auto;
-    overflow-y: hidden;
-  }
-  .scrollable {
     overflow-y: auto;
+    padding-left: var(--scrollbar-width);
   }
+  .scroll-cover {
+    position: absolute;
+    background: var(--bg-col);
+    height: 100%;  
+    top: 0;
+    right: 0;
+    width: var(--scrollbar-width);
 
-  /* Custom scrollbar */
-  ::-webkit-scrollbar {
-    width: 14px;
+    pointer-events: none;
+
+    opacity: 1;
+    transition: opacity .5s;
+    transition-delay: 1s;
+    -webkit-transition: opacity .5s;
+    -webkit-transition-delay: 1s;
   }
-  ::-webkit-scrollbar-track {
-    visibility: hidden;
-  }
-  ::-webkit-scrollbar-thumb {
-    background: #88888888;
-  }
-  ::-webkit-scrollbar-thumb:hover {
-    background: #bbbbbbbb;
+  main:hover .scroll-cover {
+    opacity: 0;
+    transition: opacity .5s;
+    -webkit-transition: opacity .5s;
   }
 </style>
