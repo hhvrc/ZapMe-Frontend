1845b95b1cee7ab8fa175cfd1f5db39cd0330027
HentaiHeavenVR <github@heavenvr.tech>
2023-02-17 18:11:05 +0100

    Add sentry for error tracking

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import "./global.css";
 import App from './App.svelte';
 
+import * as Sentry from "@sentry/svelte";
+import { BrowserTracing } from "@sentry/tracing";
+
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+const sentryTracesSampleRate = import.meta.env.VITE_SENTRY_TRACES_SAMPLE_RATE;
+
+if (sentryDsn && sentryTracesSampleRate) {
+  Sentry.init({
+    dsn: sentryDsn as string,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: sentryTracesSampleRate as number,
+  });
+}
+
 const app = new App({
   target: document.body,
 });
