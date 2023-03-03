099f1a187e7dea5602ab248f4fa8ce467ff35fc6
HentaiHeavenVR <github@heavenvr.tech>
2023-03-03 15:39:27 +0100

    rename routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  clearScreen: false,
-  resolve: {
-    alias: {
-      $api: resolve('./src/api'),
-      $cmp: resolve('./src/components'),
-      $models: resolve('./src/models'),
-      $routes: resolve('./src/routes'),
-    },
-  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-  server: {
-    port: 1337
-  },
+	}
 });
