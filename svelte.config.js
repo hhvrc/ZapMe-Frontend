d4ce1baf46d34cdb69a3cf27d3c42feb0ba7face
hhvrc <github@heavenvr.tech>
2023-03-16 11:52:00 +0100

    Fix ambient.d.ts errors

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
 import { default as cfAdapter } from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-let activeAdapter;
-
-// Read: https://developers.cloudflare.com/pages/platform/build-configuration#environment-variables
-if (process.env.CF_PAGES) {
-  // Read: https://kit.svelte.dev/docs/adapter-cloudflare
-  activeAdapter = cfAdapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-  });
-} else {
-  activeAdapter = adapter();
-}
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: activeAdapter,
+    adapter: cfAdapter({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    }),
     alias: {
       '$types': 'src/types',
       '$types/*': 'src/types/*',
