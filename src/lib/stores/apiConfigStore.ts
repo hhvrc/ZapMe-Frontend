536786664bc93f56528a2322aae5628b38701408
hhvrc <github@heavenvr.tech>
2023-04-18 22:06:03 +0200

    Initialize api config on load

--- src/lib/stores/apiConfigStore.ts
+++ src/lib/stores/apiConfigStore.ts
@@ -4,4 +4,6 @@ import { writable, type Readable } from "svelte/store";
 
 const { set, subscribe } = writable<Config | null>(null);
 
+configApi.getConfig().then(set).catch(() => set(null));
+
 export const ApiConfigStore: Readable<Config | null> = { subscribe };
