70e487b977354b4678fe610746237873926e2834
hhvrc <github@heavenvr.tech>
2023-04-18 20:11:18 +0200

    Decrease bloat from sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,23 +1,20 @@
-import * as Sentry from "@sentry/svelte";
-import { BrowserTracing } from "@sentry/tracing";
+import { init as SentryInit, captureException as SentryCaptureException } from "@sentry/svelte";
+import { BrowserTracing } from '@sentry/browser';
 
 // Initialize Sentry for error and performance monitoring
 const sentryDsn = import.meta.env.VITE_SENTRY_DSN as string | undefined;
 const sentryTracesSampleRate = import.meta.env.VITE_SENTRY_TRACES_SAMPLE_RATE as number | undefined;
 if (sentryDsn && sentryTracesSampleRate) {
-  Sentry.init({
+  SentryInit({
     dsn: sentryDsn,
     integrations: [new BrowserTracing()],
     tracesSampleRate: sentryTracesSampleRate,
   });
 }
 
-// Fetch API configuration
-
-
 export function handleError(error: any) {
   console.error(error);
   if (sentryDsn && sentryTracesSampleRate) {
-    Sentry.captureException(error);
+    SentryCaptureException(error);
   }
 }
