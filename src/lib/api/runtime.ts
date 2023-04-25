ee9528918a7aa55ec48164e4e3ff7cb7dd724124
HentaiHeavenVR <113805205+hhvrc@users.noreply.github.com>
2023-04-25 13:08:29 +0200

    Update swagger from server: add backend url

--- src/lib/api/runtime.ts
+++ src/lib/api/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "http://localhost".replace(/\/+$/, "");
+export const BASE_PATH = "https://api.zapme.app".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
