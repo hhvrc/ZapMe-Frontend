3d90fe87187e3017e716b6a0e724c9d7bd78e5ce
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-04-26 02:13:14 +0200

    and this

--- src/lib/stores/accountStore.ts
+++ src/lib/stores/accountStore.ts
@@ -1,6 +1,22 @@
-import type { AccountDto } from '$lib/api';
-import { persisted } from 'svelte-local-storage-store';
+import type { AccountDto } from "$lib/api";
+import { accountApi } from "$lib/fetchSingleton";
+import { writable } from "svelte/store";
 
-export const AccountStore = persisted<AccountDto | null>('account', null, {
-  storage: 'session',
+const store = writable({
+  account: null as AccountDto | null,
+  lastFetch: 0,
 });
+
+export const AccountStore = {
+  ...store,
+  fetch: async () => {
+    accountApi.getAccount().then((account) => {
+      store.update((_) => {
+        return {
+          account: account,
+          lastFetch: Date.now(),
+        };
+      });
+    });
+  }
+}
