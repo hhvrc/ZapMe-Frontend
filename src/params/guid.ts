3f7f95c597dbcfa292947baf9e0d83dfc9001574
HentaiHeavenVR <github@heavenvr.tech>
2023-04-27 14:23:25 +0200

    prettifty

--- src/params/guid.ts
+++ src/params/guid.ts
@@ -1,3 +1,5 @@
 export function match(param) {
-  return /^[\dA-F]{8}-[\dA-F]{4}-[4][\dA-F]{3}-[89AB][\dA-F]{3}-[\dA-F]{12}$/i.test(param);
+  return /^[\dA-F]{8}-[\dA-F]{4}-[4][\dA-F]{3}-[89AB][\dA-F]{3}-[\dA-F]{12}$/i.test(
+    param
+  );
 }
