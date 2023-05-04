e1d95bb85d0174c67730c6fa7aedb539317c90e2
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-05-05 01:07:38 +0200

    Fix dumb TLS errors

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+if (process.env.NODE_ENV === 'development') {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  console.log('\u001b[1;31mWARNING: TLS CERTIFICATE VERIFICATION DISABLED FOR DEVELOPMENT, THIS SHOULD NOT APPEAR IN PRODUCTION\u001b[0m');
+}
+
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
