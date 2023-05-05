5e8426aa3b91dd647c9b2e5311e8721366062855
hhvrc <github@heavenvr.tech>
2023-05-05 12:55:50 +0200

    eslint and prettier changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
 import { default as cfAdapter } from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
 
 export default {
   preprocess: [
-    vitePreprocess(),
     preprocess({
       postcss: true,
     }),
