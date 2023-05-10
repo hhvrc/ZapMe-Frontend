6c855252cb48ee0f7462baa52d3bab8c7bf90676
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-05-10 18:49:24 +0200

    fix verify-email

--- src/routes/verify-email/+page.svelte
+++ src/routes/verify-email/+page.svelte
@@ -1,23 +1,49 @@
 <script lang="ts">
-  export let data;
+  import { browser } from '$app/environment';
+  import { page } from '$app/stores';
+  import { AccountApi } from '$lib/api/index.js';
+  import { RuntimeApiConfiguration } from '$lib/fetchSingleton.js';
+  import { handleFetchError } from '$lib/helpers/errorDetailsHelpers';
+  
+  const accountApi = new AccountApi(RuntimeApiConfiguration);
+
+  let loading = true;
+  let error: string | null = null;
+  if (browser) {
+    const token = $page.url.searchParams.get('token');
+    if (token) {
+      accountApi.verifyEmailAddress(token)
+      .catch(async (error) => {
+        const result = await handleFetchError(error);
+        error = result?.apiCode ?? 'An unknown error occurred.';
+      })
+      .finally(() => {
+        loading = false;
+      });
+    } else {
+      error = 'Looks like the link is broken. Please try again.';
+      loading = false;
+    }
+  }
 </script>
 
 <svelte:head>
   <title>ZapMe - Verify Email Address</title>
 </svelte:head>
 
-{#if data.error}
+{#if loading}
+  <div>
+    <h1>Verifying...</h1>
+  </div>
+{:else if error}
   <div class="error">
     <h1>Oops!</h1>
-    <p>{data.error}</p>
+    <p>{error}</p>
   </div>
 {:else}
   <div class="success">
     <h1>Success!</h1>
-    <p>Your email address has been confirmed.</p>
+    <p>Your email address has been verified.</p>
     <p>Now you can <a href="/login">login</a> to your account.</p>
   </div>
 {/if}
-
-<style>
-</style>
