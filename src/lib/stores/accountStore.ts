e6a4bccb8236ff2c7781663544805d1cebbaf1a6
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-05-11 02:14:33 +0200

    Profile page improvements

--- src/lib/stores/accountStore.ts
+++ src/lib/stores/accountStore.ts
@@ -1,7 +1,25 @@
-import type { AccountDto } from '$lib/api';
+import { AccountApi, type AccountDto } from '$lib/api';
+import { RuntimeApiConfiguration } from '$lib/fetchSingleton';
+import { handleFetchError } from '$lib/helpers/errorDetailsHelpers';
 import { writable } from 'svelte/store';
 
-export const AccountStore = writable({
+const accountApi = new AccountApi(RuntimeApiConfiguration);
+
+const store = writable({
   account: null as AccountDto | null,
   lastFetch: 0,
 });
+
+function setAccount(account: AccountDto) {
+  store.update((s) => ({ ...s, account, lastFetch: Date.now() }));
+}
+
+function fetchAccount() {
+  accountApi.getAccount().then(setAccount).catch(handleFetchError);
+}
+
+export const AccountStore = {
+  ...store,
+  setAccount,
+  fetchAccount
+}
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -2,6 +2,7 @@
   import { browser } from '$app/environment';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
+  import { UserStatus } from '$lib/api';
   import { AccountStore, SessionTokenStore } from '$lib/stores';
   import { BuildRedirectURL } from '$lib/utils/redirects';
 
@@ -10,41 +11,84 @@
   }
 
   $: account = $AccountStore?.account;
+  if (!account && $SessionTokenStore) {
+    AccountStore.fetchAccount();
+  }
+
+  let onlineStatusColor = 'bg-gray-500';
+  let onlineStatusText = 'Offline';
+  
+  $: if (account) {
+    const { status } = account;
+    switch (status) {
+      case UserStatus.doNotDisturb:
+        onlineStatusColor = 'bg-red-500';
+        onlineStatusText = 'Do Not Disturb';
+        break;
+      case UserStatus.inactive:
+        onlineStatusColor = 'bg-yellow-500';
+        onlineStatusText = 'Idle';
+        break;
+      case UserStatus.online:
+        onlineStatusColor = 'bg-green-500';
+        onlineStatusText = 'Online';
+        break;
+      case UserStatus.downBad:
+        onlineStatusColor = 'bg-purple-500';
+        onlineStatusText = 'Down Bad';
+        break;
+      case UserStatus.offline:
+      default:
+        onlineStatusColor = 'bg-gray-500';
+        onlineStatusText = 'Offline';
+        break;
+    }
+  }
 </script>
 
 <svelte:head>
   <title>ZapMe - Profile</title>
 </svelte:head>
 
-<div>
-  <h1>Profile</h1>
-  {#if account}
-    <p>Id: {account.id}</p>
-    <p>Username: {account.username}</p>
-    <p>Obfuscated Email: {account.obscuredEmail}</p>
-    <p>Email Verified: {account.emailVerified}</p>
-    <p>ProfilePictureId: {account.profilePictureUrl}</p>
-    <p>Status: {account.status}</p>
-    <p>StatusText: {account.statusText}</p>
-    <p>CreatedAt: {account.createdAt}</p>
-    <p>LastOnline: {account.lastOnline}</p>
-    <p>Accepted TOS Version: {account.acceptedTermsOfServiceVersion}</p>
-    <div>
-      <h2>Friends:</h2>
-      {#each account.friends ?? [] as friend}
-        <p>{friend}</p>
-      {/each}
+{#if account}
+  <div class="float-left w-1/2 h-full py-4 pl-4 pr-2">
+    <div class="card overflow-hidden">
+      <!-- Profile banner -->
+      <img src="https://i.pinimg.com/originals/31/c3/e3/31c3e3f032a177d0b0c983b8262de8f9.gif" alt="ProfileBanner" class="w-full h-32 object-cover select-none"/>
+      
+      <div class="flex justify-start place-items-start px-4 py-2 h-12 mb-4">
+        <!-- Profile picture -->
+        <img src="https://i.pinimg.com/originals/49/07/13/4907130d5ddcd50beff46f78c9744a49.png" alt="ProfilePicture" class="rounded-full relative -top-16 w-28 h-28 border-4 border-surface-800 select-none" />
+      
+        <!-- Profile name -->
+        <h2 class="text-2xl font-bold">{account.username}</h2>
+        
+        <!-- Profile status (place at end center) -->
+        <div class="flex items-center ml-auto text-sm">
+          <div class={`w-3 h-3 rounded-full ${onlineStatusColor} mr-2`}></div>
+          <p class="text-sm">{onlineStatusText}</p>
+        </div>
+      </div>
+      <div class="bg-surface-900 m-4 p-4 rounded-lg">
+        <h3 class="text-xl font-bold">About</h3>
+        <p>Status: {account.statusText}</p>
+        <p>Created: {new Date(account.createdAt).toLocaleString()}</p>
+      </div>
+      <hr>
+      <div class="p-4">
+        <h5>Connected OAuth Accounts:</h5>
+        {#each account.oauthConnections ?? [] as oauthConnection}
+          <p>{oauthConnection}</p>
+        {/each}
+      </div>
     </div>
-    <div>
-      <h2>Connected OAuth Accounts:</h2>
-      {#each account.oauthConnections ?? [] as oauthConnection}
-        <p>{oauthConnection}</p>
-      {/each}
+  </div>
+  <div class="float-right w-1/2 h-full py-4 pl-2 pr-4">
+    <div class="card p-3">
+      <h1>Settings</h1>
+      <p>Coming soon...</p>
     </div>
-  {:else}
-    <p>Not signed in.</p>
-  {/if}
-</div>
-
-<style>
-</style>
+  </div>
+{:else}
+  <p>Loading...</p>
+{/if}
