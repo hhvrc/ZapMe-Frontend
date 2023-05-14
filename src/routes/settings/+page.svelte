91f8213e18372a3aea68fd6f990fb8af9cb5fb35
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-05-14 10:32:29 +0200

    Start working on settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,8 +1,44 @@
+<script lang="ts">
+  import TextInput from "$components/TextInput.svelte";
+import { AccountStore, SessionTokenStore } from "$lib/stores";
+
+  $: account = $AccountStore?.account;
+  if (!account && $SessionTokenStore) {
+    AccountStore.fetchAccount();
+  }
+
+  let username = '';
+</script>
+
 
 <svelte:head>
   <title>ZapMe - Settings</title>
 </svelte:head>
 
-<div>
-  <h1>Settings</h1>
-</div>
+{#if account}
+  <div class="py-4 px-4">
+    <div class="card p-4 overflow-hidden">
+      <span class="text-2xl font-bold">Settings</span>
+      <div class="flex">
+        <input type="text" class="input w-full" placeholder="Username" bind:value={username} />
+        <button type="button" class="btn variant-filled w-1/2 self-center">
+          <span>Edit</span>
+        </button>
+      </div>
+      <div class="flex">
+        <input type="text" class="input w-full" placeholder="Email" bind:value={username} />
+        <button type="button" class="btn variant-filled w-1/2 self-center">
+          <span>Edit</span>
+        </button>
+      </div>
+      <div class="flex">
+        <input type="text" class="input w-full" placeholder="Password" bind:value={username} />
+        <button type="button" class="btn variant-filled w-1/2 self-center">
+          <span>Edit</span>
+        </button>
+      </div>
+    </div>
+  </div>
+{:else}
+  <p>Loading...</p>
+{/if}
