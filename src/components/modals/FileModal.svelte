f7e39a9c3ec88aeb90d4cfebdc504f6bd2c4db22
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-05-18 06:10:08 +0200

    prettier

--- src/components/modals/FileModal.svelte
+++ src/components/modals/FileModal.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-  import { FileDropzone, modalStore } from "@skeletonlabs/skeleton";
+  import { FileDropzone, modalStore } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="modal block bg-surface-100-800-token w-modal h-auto p-4 space-y-4 rounded-container-token shadow-xl">
-  <header class="modal-header text-2xl font-bold grid grid-cols-[1fr_1fr_auto]">
+<div
+  class="modal w-modal block h-auto space-y-4 p-4 shadow-xl bg-surface-100-800-token rounded-container-token"
+>
+  <header class="modal-header grid grid-cols-[1fr_1fr_auto] text-2xl font-bold">
     Select an Image
-    <div/>
+    <div />
     <button type="button" class="btn variant-ghost-surface">Cancel</button>
   </header>
   <!-- Upload file, giphy, or tenor -->
   <FileDropzone name="files">
-    <svelte:fragment slot="lead"><i class="fa-solid fa-file-upload fa-xl"/></svelte:fragment>
-    <svelte:fragment slot="message"><b>Upload a image</b> or drag an drop</svelte:fragment>
+    <svelte:fragment slot="lead"
+      ><i class="fa-solid fa-file-upload fa-xl" /></svelte:fragment
+    >
+    <svelte:fragment slot="message"
+      ><b>Upload a image</b> or drag an drop</svelte:fragment
+    >
     <svelte:fragment slot="meta">PNG, JPG, and GIF allowed.</svelte:fragment>
   </FileDropzone>
   <div class="grid grid-cols-[1fr_auto_1fr] items-center gap-3 text-gray-500">
@@ -20,7 +26,19 @@
     <hr />
   </div>
   <div class="grid grid-cols-[1fr_1fr] items-center gap-3 text-gray-500">
-    <button type="button" class="btn variant-filled-surface" on:click={() => modalStore.trigger({ type: 'component', component: 'giphy' })}>GIPHY</button>
-    <button type="button" class="btn variant-filled-surface" on:click={() => modalStore.trigger({ type: 'component', component: 'tenor' })}>TENOR</button>
+    <button
+      type="button"
+      class="btn variant-filled-surface"
+      on:click={() =>
+        modalStore.trigger({ type: 'component', component: 'giphy' })}
+      >GIPHY</button
+    >
+    <button
+      type="button"
+      class="btn variant-filled-surface"
+      on:click={() =>
+        modalStore.trigger({ type: 'component', component: 'tenor' })}
+      >TENOR</button
+    >
   </div>
 </div>
--- src/components/modals/TenorModal.svelte
+++ src/components/modals/TenorModal.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 </script>
 
-<div class="modal block bg-surface-100-800-token w-modal h-auto p-4 space-y-4 rounded-container-token shadow-xl">
-  <header class="modal-header text-2xl font-bold grid grid-cols-[1fr_1fr_auto]">
+<div
+  class="modal w-modal block h-auto space-y-4 p-4 shadow-xl bg-surface-100-800-token rounded-container-token"
+>
+  <header class="modal-header grid grid-cols-[1fr_1fr_auto] text-2xl font-bold">
     Select a GIF
-    <div/>
+    <div />
     <button type="button" class="btn variant-ghost-surface">Cancel</button>
   </header>
 </div>
