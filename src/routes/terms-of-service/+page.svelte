a1cd86c901bd437bebb832cfdae81ee8cc9fd2fe
hhvrc <github@heavenvr.tech>
2023-06-19 02:14:33 +0200

    More robust markdown rendering

--- src/routes/terms-of-service/+page.svelte
+++ src/routes/terms-of-service/+page.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
+  import { renderMarkdown } from '$lib/utils/renderMarkdown';
+  import { ApiConfigStore } from '$lib/stores';
+
   /* eslint-disable svelte/no-at-html-tags */
 
-  import { ApiConfigStore } from '$lib/stores';
-  import showdown from 'showdown';
-
-  const converter = new showdown.Converter({
-    customizedHeaderId: true,
-    requireSpaceBeforeHeadingText: true,
-    openLinksInNewWindow: true,
-    underline: true,
-    metadata: true,
-  });
+  $: markdown = $ApiConfigStore?.termsOfServiceMarkdown ?? '';
+  $: rendered = renderMarkdown(markdown);
 </script>
 
 <svelte:head>
   <title>ZapMe - Terms Of Service</title>
 </svelte:head>
 
-{#if browser}
-  <div class="p-2 lg:p-4">
-    <!-- There is no risk of XSS here, as the markdown can only be set by a system administrator -->
-    {@html converter.makeHtml($ApiConfigStore?.privacyPolicyMarkdown ?? '')}
-  </div>
-{/if}
+<div class="p-2 lg:p-4">
+  <!-- There is a risk of XSS here, but it is acceptable because the markdown can only be set by a system administrator -->
+  {@html rendered}
+</div>
