f281e9ef3d737405966ff88729a6a3b98aebc73e
hhvrc <github@heavenvr.tech>
2023-06-30 01:48:32 +0200

    Sentry logging improvements

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,22 +1,38 @@
 import { BrowserTracing, Replay } from '@sentry/browser';
 import { captureException as SentryCaptureException, init as SentryInit } from '@sentry/svelte';
 import {
-  PUBLIC_REPLAYS_ONERROR_SAMPLERATE,
-  PUBLIC_REPLAYS_SESSION_SAMPLERATE,
   PUBLIC_SENTRY_DSN,
+  PUBLIC_SENTRY_ENABLED,
+  PUBLIC_SENTRY_REPLAYS_ONERROR_SAMPLERATE,
+  PUBLIC_SENTRY_REPLAYS_SESSION_SAMPLERATE,
   PUBLIC_SENTRY_TRACES_SAMPLERATE,
 } from '$env/static/public';
 
-// Initialize Sentry for error and performance monitoring
-SentryInit({
-  dsn: PUBLIC_SENTRY_DSN,
-  integrations: [new BrowserTracing(), new Replay()],
-  tracesSampleRate: parseFloat(PUBLIC_SENTRY_TRACES_SAMPLERATE),
-  replaysSessionSampleRate: parseFloat(PUBLIC_REPLAYS_SESSION_SAMPLERATE),
-  replaysOnErrorSampleRate: parseFloat(PUBLIC_REPLAYS_ONERROR_SAMPLERATE),
-});
+let errorHandler: any;
+if (PUBLIC_SENTRY_ENABLED) {
+  // Initialize Sentry for error and performance monitoring
+  SentryInit({
+    dsn: PUBLIC_SENTRY_DSN,
+    tracesSampleRate: parseFloat(PUBLIC_SENTRY_TRACES_SAMPLERATE),
 
-export function handleError(error) {
-  console.error(error);
-  SentryCaptureException(error);
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: parseFloat(PUBLIC_SENTRY_REPLAYS_SESSION_SAMPLERATE),
+
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: parseFloat(PUBLIC_SENTRY_REPLAYS_ONERROR_SAMPLERATE),
+
+    // If you don't want to use Session Replay, just remove the line below:
+    integrations: [new BrowserTracing(), new Replay()],
+  });
+  errorHandler = SentryCaptureException;
+} else {
+  console.log('Sentry disabled in dev mode');
+  errorHandler = (e) => {
+    console.error(e);
+  };
 }
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = errorHandler;
