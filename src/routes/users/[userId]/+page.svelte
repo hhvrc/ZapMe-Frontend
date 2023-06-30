dfe04ca1a96093515673ed7a141e34926e297864
hhvrc <github@heavenvr.tech>
2023-06-30 10:11:18 +0200

    Debug prod error

--- src/routes/users/[userId]/+page.svelte
+++ src/routes/users/[userId]/+page.svelte
@@ -1,6 +1,5 @@
+<!--
 <script lang="ts">
-  import { ProgressRadial } from '@skeletonlabs/skeleton';
-  import { browser } from '$app/environment';
   import { page } from '$app/stores';
   import ResponsiveCard from '$components/ResponsiveCard.svelte';
   import UserProfileFetcher from '$components/User/UserProfileFetcher.svelte';
@@ -8,10 +7,7 @@
   const userId = $page.params.userId;
 </script>
 
-{#if browser}
-  <ResponsiveCard>
-    <UserProfileFetcher {userId} />
-  </ResponsiveCard>
-{:else}
-  <ProgressRadial />
-{/if}
+<ResponsiveCard>
+  <UserProfileFetcher {userId} />
+</ResponsiveCard>
+-->
