13b561ee8952f523f3610cf13018afdac2c29c7d
hhvrc <github@heavenvr.tech>
2023-07-04 21:57:26 +0200

    [Chore] Prettier

--- src/routes/verify-email/+page.svelte
+++ src/routes/verify-email/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+  import { ProgressBar } from '@skeletonlabs/skeleton';
   import { browser } from '$app/environment';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { accountApi } from '$lib/fetchSingleton';
   import { SessionTokenStore } from '$lib/stores';
-  import { ProgressBar } from '@skeletonlabs/skeleton';
 
   const token = browser ? $page.url.searchParams.get('token') : null;
 </script>
@@ -13,20 +13,24 @@
   <title>ZapMe - Verify Email Address</title>
 </svelte:head>
 
-<div class="card responsive-card p-8 flex flex-col gap-8 items-center">
+<div class="responsive-card card flex flex-col items-center gap-8 p-8">
   {#if browser}
     {#if token}
       {#await accountApi.completeAccountEmailVerification(token)}
         <h2>Verifying your email address...</h2>
         <ProgressBar />
-      {:then} 
+      {:then}
         <h2>Email Verified</h2>
         {#if $SessionTokenStore}
           <h4>Your email address has been verified.</h4>
-          <button class="btn btn-primary variant-filled" on:click={() => goto('/')}>Go to Home</button>
+          <button class="btn-primary btn variant-filled" on:click={() => goto('/')}
+            >Go to Home</button
+          >
         {:else}
           <h4>You can now login to your account.</h4>
-          <button class="btn btn-primary variant-filled" on:click={() => goto('/login')}>Go to Login</button>
+          <button class="btn-primary btn variant-filled" on:click={() => goto('/login')}
+            >Go to Login</button
+          >
         {/if}
       {:catch}
         <h2>Email Verification Failed</h2>
