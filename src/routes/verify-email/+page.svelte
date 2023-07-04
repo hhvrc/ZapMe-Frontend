5b286b3550129a46f5df2ad51900986be0b3288a
hhvrc <github@heavenvr.tech>
2023-07-04 20:39:11 +0200

    Fix build

--- src/routes/verify-email/+page.svelte
+++ src/routes/verify-email/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { accountApi } from '$lib/fetchSingleton';
   import { ProgressBar } from '@skeletonlabs/skeleton';
 
-  const token = $page.url.searchParams.get('token');
+  const token = browser ? $page.url.searchParams.get('token') : null;
 </script>
 
 <svelte:head>
@@ -12,20 +13,24 @@
 </svelte:head>
 
 <div class="card responsive-card p-8 flex flex-col gap-8 items-center">
-  {#if token}
-    {#await accountApi.completeAccountEmailVerification(token)}
-      <h2>Verifying your email address...</h2>
-      <ProgressBar />
-    {:then} 
-      <h2>Email Verified</h2>
-      <h4>You can now login to your account.</h4>
-      <button class="btn btn-primary variant-filled" on:click={() => goto('/login')}>Go to Login</button>
-    {:catch}
-      <h2>Email Verification Failed</h2>
+  {#if browser}
+    {#if token}
+      {#await accountApi.completeAccountEmailVerification(token)}
+        <h2>Verifying your email address...</h2>
+        <ProgressBar />
+      {:then} 
+        <h2>Email Verified</h2>
+        <h4>You can now login to your account.</h4>
+        <button class="btn btn-primary variant-filled" on:click={() => goto('/login')}>Go to Login</button>
+      {:catch}
+        <h2>Email Verification Failed</h2>
+        <p>Please check the link in your email and try again.</p>
+      {/await}
+    {:else}
+      <h2>Invalid Link</h2>
       <p>Please check the link in your email and try again.</p>
-    {/await}
+    {/if}
   {:else}
-    <h2>Email Verification Failed</h2>
-    <p>This link is broken, please check your email and try again.</p>
+    <ProgressBar />
   {/if}
 </div>
