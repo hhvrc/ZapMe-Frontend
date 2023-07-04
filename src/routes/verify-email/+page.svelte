094829712becc660af8569f8ac02761653f142f9
hhvrc <github@heavenvr.tech>
2023-07-04 20:30:26 +0200

    Improve email verification page UI

--- src/routes/verify-email/+page.svelte
+++ src/routes/verify-email/+page.svelte
@@ -1,47 +1,31 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
+  import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { accountApi } from '$lib/fetchSingleton';
-  import { handleFetchError } from '$lib/helpers/errorDetailsHelpers';
+  import { ProgressBar } from '@skeletonlabs/skeleton';
 
-  let loading = true;
-  let error: string | null = null;
-  if (browser) {
-    const token = $page.url.searchParams.get('token');
-    if (token) {
-      accountApi
-        .completeAccountEmailVerification(token)
-        .catch(async (error) => {
-          const result = await handleFetchError(error);
-          error = result?.apiCode ?? 'An unknown error occurred.';
-        })
-        .finally(() => {
-          loading = false;
-        });
-    } else {
-      error = 'Looks like the link is broken. Please try again.';
-      loading = false;
-    }
-  }
+  const token = $page.url.searchParams.get('token');
 </script>
 
 <svelte:head>
   <title>ZapMe - Verify Email Address</title>
 </svelte:head>
 
-{#if loading}
-  <div>
-    <h1>Verifying...</h1>
-  </div>
-{:else if error}
-  <div class="error">
-    <h1>Oops!</h1>
-    <p>{error}</p>
-  </div>
-{:else}
-  <div class="success">
-    <h1>Success!</h1>
-    <p>Your email address has been verified.</p>
-    <p>Now you can <a href="/login">login</a> to your account.</p>
-  </div>
-{/if}
+<div class="card responsive-card p-8 flex flex-col gap-8 items-center">
+  {#if token}
+    {#await accountApi.completeAccountEmailVerification(token)}
+      <h2>Verifying your email address...</h2>
+      <ProgressBar />
+    {:then} 
+      <h2>Email Verified</h2>
+      <h4>You can now login to your account.</h4>
+      <button class="btn btn-primary variant-filled" on:click={() => goto('/login')}>Go to Login</button>
+    {:catch}
+      <h2>Email Verification Failed</h2>
+      <p>Please check the link in your email and try again.</p>
+    {/await}
+  {:else}
+    <h2>Email Verification Failed</h2>
+    <p>This link is broken, please check your email and try again.</p>
+  {/if}
+</div>
