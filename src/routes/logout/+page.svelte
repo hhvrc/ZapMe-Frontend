f210280068c836a0d4fe8e751e113c6bec78b377
hhvrc <github@heavenvr.tech>
2023-07-14 16:05:26 +0200

    Improve logout page

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { authenticationApi } from '$lib/fetchSingleton';
   import { SessionTokenStore } from '$lib/stores';
   import { GetRedirectURL } from '$lib/utils/redirects';
-  import { onMount } from 'svelte';
+  import { toastStore } from '@skeletonlabs/skeleton';
 
-  onMount(async () => {
+  const loggedIn = !!$SessionTokenStore;
+
+  function AfterApiSignOut() {
     try {
-      await authenticationApi.signOut();
-    } finally {
       SessionTokenStore.set(null);
+      if (loggedIn) {
+        toastStore.trigger({
+          message: 'Signed out',
+          autohide: true,
+          timeout: 3000,
+          background: 'variant-filled-success'
+        });
+      }
+    } finally {
       goto(GetRedirectURL($page.url, '/'));
     }
-  });
+  }
+
+  if (browser) {
+    if (loggedIn) {
+      authenticationApi.signOut().finally(AfterApiSignOut);
+    } else {
+      goto(GetRedirectURL($page.url, '/'));
+    }
+  }
 </script>
