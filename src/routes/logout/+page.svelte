99906793095588c12791c4482b27663d78f73eed
hhvrc <github@heavenvr.tech>
2023-07-14 16:08:31 +0200

    Simplify logout logic

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,32 +1,28 @@
 <script lang="ts">
+  import { toastStore } from '@skeletonlabs/skeleton';
   import { browser } from '$app/environment';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { authenticationApi } from '$lib/fetchSingleton';
   import { SessionTokenStore } from '$lib/stores';
   import { GetRedirectURL } from '$lib/utils/redirects';
-  import { toastStore } from '@skeletonlabs/skeleton';
-
-  const loggedIn = !!$SessionTokenStore;
 
   function AfterApiSignOut() {
     try {
       SessionTokenStore.set(null);
-      if (loggedIn) {
-        toastStore.trigger({
-          message: 'Signed out',
-          autohide: true,
-          timeout: 3000,
-          background: 'variant-filled-success'
-        });
-      }
+      toastStore.trigger({
+        message: 'Signed out',
+        autohide: true,
+        timeout: 3000,
+        background: 'variant-filled-success',
+      });
     } finally {
       goto(GetRedirectURL($page.url, '/'));
     }
   }
 
   if (browser) {
-    if (loggedIn) {
+    if ($SessionTokenStore) {
       authenticationApi.signOut().finally(AfterApiSignOut);
     } else {
       goto(GetRedirectURL($page.url, '/'));
